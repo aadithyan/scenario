5d5ac19af2441c2d89b01c66aa9456d2347ef0e2
Aadithyan S <aadithyan.s@rubyians.com>
2019-06-10 16:57:31 +0530

    Adding Seeds to populate Levels

--- scenario_api/db/seeds.rb
+++ scenario_api/db/seeds.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Seeds
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+Api::V1::Level.find_or_create_by(name: 'Beginner', active: true)
+Api::V1::Level.find_or_create_by(name: 'Amatuer', active: true)
+Api::V1::Level.find_or_create_by(name: 'Intermediate', active: true)
+Api::V1::Level.find_or_create_by(name: 'Expert', active: true)
