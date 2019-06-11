fff1b4a0194c9bdf29e1b065aee01a5ab574cbc2
Aadithyan S <aadithyan.s@rubyians.com>
2019-06-11 12:09:22 +0530

    Adding user detail api

--- scenario_api/spec/integration/api/v1/users_spec.rb
+++ scenario_api/spec/integration/api/v1/users_spec.rb
@@ -57,6 +57,66 @@ describe 'Users' do
     end
   end
 
+  path '/api/v1/users/{id}' do
+    get 'Get Single User details' do
+      tags 'Users'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter in: :header, type: :string, name: 'content-type',
+                required: true, description: 'Content Type'
+      parameter in: :header, type: :string, name: 'Authorization',
+                required: true, description: 'Bearer <token>'
+      parameter in: :path, type: :string, name: :id,
+                required: true, description: 'User Id'
+      response 200, 'Gets single user details response' do
+        schema type: :object,
+               properties: {
+                 data: {
+                   properties: {
+                     id: { type: :integer },
+                     type: { type: :string },
+                     attributes: {
+                       properties: {
+                         id: { type: :integer },
+                         first_name: { type: :string },
+                         middle_name: { type: :string },
+                         last_name: { type: :string },
+                         email: { type: :string },
+                         password: { type: :string },
+                         authentiation_token: { type: :string },
+                         user_name: { type: :string },
+                         work_email: { type: :string },
+                         gender: { type: :string },
+                         active: { type: :boolean },
+                         about_me: { type: :string },
+                         dob: { type: :date },
+                         address: { type: :string },
+                         city: { type: :string },
+                         state: { type: :string },
+                         zip_postal_code: { type: :string },
+                         country: { type: :string },
+                         contact_no: { type: :string },
+                         emergency_contact_no: { type: :string },
+                         nationality: { type: :string },
+                         marital_status: { type: :string }
+                       }
+                     }
+                   }
+                 }
+               }
+        run_test!
+      end
+      response 409, 'Failed to update user' do
+        schema type: :object,
+               properties: {
+                 status: { type: :string },
+                 message: { type: :string }
+               }
+        run_test!
+      end
+    end
+  end
+
   path '/api/v1/users' do
     post 'Creates new user' do
       tags 'Users'
