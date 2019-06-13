1040f64367bc9980a26b1d42ea24d17e6497af73
Aadithyan S <aadithyan.s@rubyians.com>
2019-06-13 15:57:49 +0530

    Adding package show api

--- scenario_api/spec/integration/api/v1/package_spec.rb
+++ scenario_api/spec/integration/api/v1/package_spec.rb
@@ -87,4 +87,88 @@ describe 'Packages' do
       end
     end
   end
+
+  path '/api/v1/packages/{id}' do
+    get 'List single package' do
+      tags 'Packages'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter in: :header, type: :string, name: 'content-type',
+                required: true, description: 'Content Type'
+      parameter in: :header, type: :string, name: 'Authorization',
+                required: true, description: 'Bearer <token>'
+      parameter in: :path, type: :string, name: :id,
+                required: true, description: 'Package Id'
+      response 200, 'Response for listing single package' do
+        schema type: :object,
+               properties: {
+                 data: {
+                   type: :object,
+                   properties: {
+                     id: { type: :integer },
+                     type: { type: :string },
+                     attributes: {
+                       properties: {
+                         name: { type: :string },
+                         product_code: { type: :string },
+                         package_categories_id: { type: :integer },
+                         description: { type: :string }
+                       }
+                     },
+                     relationships: {
+                       type: :object,
+                       properties: {
+                         package_categories: {
+                           type: :object,
+                           properties: {
+                             data: {
+                               type: :object,
+                               properties: {
+                                 id: { type: :integer },
+                                 type: { type: :string }
+                               }
+                             }
+                           }
+                         }
+                       }
+                     }
+                   }
+                 },
+                 included: {
+                   type: :array,
+                   items: {
+                     properties: {
+                       id: { type: :integer },
+                       type: { type: :string },
+                       attributes: {
+                         properties: {
+                           name: { type: :string },
+                           description: { type: :string }
+                         }
+                       },
+                       relationships: {
+                         properties: {
+                           packages: {
+                             properties: {
+                               data: {
+                                 type: :array,
+                                 items: {
+                                   properties: {
+                                     id: { type: :integer },
+                                     type: { type: :string }
+                                   }
+                                 }
+                               }
+                             }
+                           }
+                         }
+                       }
+                     }
+                   }
+                 }
+               }
+        run_test!
+      end
+    end
+  end
 end
